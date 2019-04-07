b28a68e634feadca9019071ab4615a4dee167d84
hhchak <henrychak826@gmail.com>
2019-04-07 13:24:59 +0800

    Sign up page

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { SignupPage } from '../signup/signup';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['signin.page.scss'],
 })
 export class SigninPage {
-
+  constructor(
+    public navCtrl: NavController,
+    public authService: AuthService
+  ) {
+    this.loading = false;
+    //this.items = db.list('chatrooms').valueChanges();
+  }
+  goToSignup() {
+    this.navCtrl.push(SignupPage);
+  }
 }
